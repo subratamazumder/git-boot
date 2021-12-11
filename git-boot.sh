bf064ed6957e4dba89c80ed5d82473ea5dd63710
Subrata Mazumder <subrata.besu@gmail.com>
2021-12-11 13:27:50 +0000

    lincense added

--- git-boot.sh
+++ git-boot.sh
@@ -4,6 +4,7 @@ mkdir -p $WORKSPACE/$1
 cd $WORKSPACE/$1
 git init
 echo $1 > README.md
+wget https://raw.githubusercontent.com/subratamazumder/git-boot/master/LICENSE
 git add .
 git commit -m "readme added" .
 hub create
